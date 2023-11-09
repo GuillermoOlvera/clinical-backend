b9b6988c40a4f68d839795a82a59a3b644e26b89
Guillermo Olvera López <73192041+GuillermoOlvera@users.noreply.github.com>
2023-11-09 00:23:31 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+// Configurar CORS
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -10,9 +15,10 @@ const loginRoutes = require("./routes/login");
 const updateRoutes = require("./routes/update");
 const addBeneficiaryRoutes = require("./routes/addBeneficiary");
 const deleteBeneficiary = require("./routes/deleteBeneficiary");
+const getBeneficiaries = require("./routes/getBeneficiary");
+const getPatientData = require("./routes/getPatientData");
 
 // Rutas //
-
 // Ruta de inicio de sesión
 app.post("/login", loginRoutes);
 // Ruta para actualizar el expediente médico
@@ -21,6 +27,10 @@ app.put("/actualizar-expediente", updateRoutes);
 app.post("/agregar-beneficiario", addBeneficiaryRoutes);
 // Ruta para eliminar un beneficiario
 app.delete('/eliminar-beneficiario/:beneficiarioId', deleteBeneficiary);
+// Ruta para obtener la lista de beneficiarios
+app.get("/getBeneficiaries", getBeneficiaries);
+// Ruta para obtener los datos del paciente
+app.get("/getPatientData", getPatientData);
 
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto ${port}`);
